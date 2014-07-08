3c62ffe86dc4fd96c25af99fa78668e6a1dbf91a
Sammyfrw <sammyfrw@gmail.com>
2014-07-08 11:06:25 -0400

    Users can create new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,21 @@ class TasksController < ApplicationController
   before_action :require_login 
   def index
     @tasks = current_user.tasks
+    @task = Task.new
   end
+
+  def create
+    @task = current_user.tasks.create(task_params)
+    redirect_to :tasks
+  end
+
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :body)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :tasks, only: [:index, :create]
 
 
   get "/sign_up" => "users#new", as: :sign_up
